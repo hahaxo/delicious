d6a071a722abc841e5857d6081b656a470bca657
hahaxo <hahaxo@126.com>
2017-09-21 13:47:26 +0800

    add meta

--- header.php
+++ header.php
@@ -7,6 +7,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Delicious Bookmark</title>
+  <meta name="description" content="个人网站收藏夹（网址收藏）服务，用来代替被下线的美味书签(del.ici.ous)。我们的域名:hahaxo.com">
+  <meta name="keywords" content="网址收藏,书签,社会化书签,网站收藏,个人书签,个人网址,收藏夹,浏览器收藏夹,Delicious,bookmark">
   <!-- Favicon -->
   <link rel="apple-touch-icon" sizes="180x180" href="http://hahaxo.com/delicious/img/favicons/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="http://hahaxo.com/delicious/img/favicons/favicon-32x32.png">
